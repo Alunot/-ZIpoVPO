b4ed9dd4abea9864963d1bc73b0c8b6728149513
Alexandr Gladkov <50058396+Alunot@users.noreply.github.com>
2020-06-14 12:31:40 +0600

    Add files via upload

---
+++ ConsoleApp1/IObjectContent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Antimalware.DomainObjects.ScanObjectAbstraction
+{
+    public interface IObjectContent
+    {
+        ulong Size { get; }
+        bool Read(ulong Position, byte[] data, out uint readBytesCount);
+    }
+}
